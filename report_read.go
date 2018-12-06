dfc1f714666d9020254f6d912c9531fe1fecb62d
Sam Kleinman <sam@10gen.com>
2018-12-06 17:12:27 -0500

    MAKE-554: use alternate bson library

--- report_read.go
+++ report_read.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/pkg/errors"
-	"gopkg.in/mgo.v2/bson"
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -15,8 +14,6 @@ type unmarshaler func([]byte, interface{}) error
 
 func getUnmarshaler(fn string) unmarshaler {
 	switch {
-	case strings.HasSuffix(fn, ".bson"):
-		return bson.Unmarshal
 	case strings.HasSuffix(fn, ".json"):
 		return json.Unmarshal
 	case strings.HasSuffix(fn, ".yaml"), strings.HasSuffix(fn, ".yml"):
