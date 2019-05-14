10dca7f9505bf9fb8049dff3623ac0bb42566d82
julianedwards <julian.edwards@mongodb.com>
2019-05-14 09:43:50 -0400

    fix test for real

--- rpc/client_test.go
+++ rpc/client_test.go
@@ -221,7 +221,7 @@ func TestClient(t *testing.T) {
 				require.NoError(t, artifact.Validate())
 				expectedArtifact := internal.ExportArtifactInfo(&artifact)
 				expectedArtifact.Location = internal.StorageLocation_CEDAR_S3
-				assert.Equal(t, internal.ExportArtifactInfo(&artifact), result.Artifacts[j])
+				assert.Equal(t, expectedArtifact, result.Artifacts[j])
 				r, err := s3Bucket.Get(ctx, artifact.Path)
 				require.NoError(t, err)
 				remoteData, err := ioutil.ReadAll(r)
